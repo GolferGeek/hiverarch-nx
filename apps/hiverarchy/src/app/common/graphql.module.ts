d1394301b3082964d497985d6a395b178ed4c72e
golfergeek <golfergeet@gmail.com>
2022-11-06 13:35:06 -0600

    upgrade all yarns

--- apps/hiverarchy/src/app/common/graphql.module.ts
+++ apps/hiverarchy/src/app/common/graphql.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ApolloDriver } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
+      driver: ApolloDriver,
       autoSchemaFile: './schema.gql',
       debug: true,
       playground: true,
